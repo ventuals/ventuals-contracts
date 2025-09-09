efe94664df5503aee411ac2b06e7be343601be9c
Emily Hsia <emily.j.hsia@gmail.com>
2025-09-08 23:12:02 -0400

    feat: Update vHYPE to be UUPSUpgradeable and use ProtocolRegistry (#12)

--- src/vHYPE.sol
+++ src/vHYPE.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.4.0
 pragma solidity ^0.8.27;
 
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
@@ -9,51 +8,44 @@ import {ERC20BurnableUpgradeable} from
 import {ERC20PausableUpgradeable} from
     "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {ProtocolRegistry} from "./ProtocolRegistry.sol";
 
-contract vHYPE is
-    Initializable,
-    ERC20Upgradeable,
-    ERC20BurnableUpgradeable,
-    ERC20PausableUpgradeable,
-    AccessControlUpgradeable
-{
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+contract vHYPE is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, UUPSUpgradeable {
+    ProtocolRegistry public protocolRegistry;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(address defaultAdmin, address pauser, address minter) public initializer {
+    function initialize(address _protocolRegistry) public initializer {
         __ERC20_init("vHYPE", "vHYPE");
         __ERC20Burnable_init();
-        __ERC20Pausable_init();
-        __AccessControl_init();
 
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(PAUSER_ROLE, pauser);
-        _grantRole(MINTER_ROLE, minter);
+        protocolRegistry = ProtocolRegistry(_protocolRegistry);
     }
 
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
-
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
-
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    function mint(address to, uint256 amount) public onlyManager {
         _mint(to, amount);
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20Upgradeable, ERC20PausableUpgradeable)
-    {
-        super._update(from, to, value);
+    function burn(uint256 amount) public override {
+        _burn(msg.sender, amount);
     }
+
+    modifier onlyManager() {
+        require(protocolRegistry.hasRole(protocolRegistry.MANAGER_ROLE(), msg.sender), "Caller is not a manager");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(protocolRegistry.owner() == msg.sender, "Caller is not the owner");
+        _;
+    }
+
+    /// @notice Authorizes an upgrade. Only the owner can authorize an upgrade.
+    /// @dev DO NOT REMOVE THIS FUNCTION, OTHERWISE WE LOSE THE ABILITY TO UPGRADE THE CONTRACT
+    /// @param newImplementation The address of the new implementation
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 }
