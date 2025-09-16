5d78a553ac75447742a123963e61b00ec093c19d
Emily Hsia <emily.j.hsia@gmail.com>
2025-09-16 10:29:05 -0400

    chore: Remove burn() from vHYPE (#47)

--- src/VHYPE.sol
+++ src/VHYPE.sol
@@ -2,11 +2,9 @@
 pragma solidity ^0.8.27;
 
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import {ERC20BurnableUpgradeable} from
-    "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 import {Base} from "./Base.sol";
 
-contract VHYPE is Base, ERC20Upgradeable, ERC20BurnableUpgradeable {
+contract VHYPE is Base, ERC20Upgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -14,15 +12,10 @@ contract VHYPE is Base, ERC20Upgradeable, ERC20BurnableUpgradeable {
 
     function initialize(address _roleRegistry) public initializer {
         __ERC20_init("vHYPE", "vHYPE");
-        __ERC20Burnable_init();
         __Base_init(_roleRegistry);
     }
 
     function mint(address to, uint256 amount) public onlyManager {
         _mint(to, amount);
-    }
-
-    function burn(uint256 amount) public override {
-        _burn(msg.sender, amount);
     }
 }
