7ee7e5e5d9f35da9ed5c6457236e75e5453eb9ca
emilyhsia <emily.j.hsia@gmail.com>
2025-09-18 15:56:11 -0400

    Revert "chore: Move one-block delay logic into StakingVault"
    This reverts commit 024ef1373415baa4dd1621e463b3f34d31d163a8.

--- src/StakingVault.sol
+++ src/StakingVault.sol
@@ -7,21 +7,11 @@ import {L1ReadLibrary} from "./libraries/L1ReadLibrary.sol";
 import {Base} from "./Base.sol";
 
 contract StakingVault is IStakingVault, Base {
-    /// @dev The HYPE system address
     address public immutable HYPE_SYSTEM_ADDRESS = 0x2222222222222222222222222222222222222222;
 
-    /// @dev The HYPE token ID; differs between mainnet (150) and testnet (1105) (see https://hyperliquid.gitbook.io/hyperliquid-docs/for-developers/api/asset-ids)
-    uint64 public immutable HYPE_TOKEN_ID;
-
-    /// @dev The last block number when HYPE was transferred from HyperEVM to HyperCore
-    /// @dev Used to enforce a one-block delay between HyperEVM -> HyperCore transfers and deposits
-    uint256 public lastEvmToCoreTransferBlockNumber;
-
     /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor(uint64 _hypeTokenId) {
+    constructor() {
         _disableInitializers();
-
-        HYPE_TOKEN_ID = _hypeTokenId;
     }
 
     function initialize(address _roleRegistry) public initializer {
@@ -55,8 +45,6 @@ contract StakingVault is IStakingVault, Base {
 
     /// @inheritdoc IStakingVault
     function transferHypeToCore(uint256 amount) external onlyManager whenNotPaused {
-        require(block.number > lastEvmToCoreTransferBlockNumber, CannotTransferToCoreUntilNextBlock());
-
         // This is an important safety check - ensures that the StakingVault account is activated on HyperCore.
         // If the StakingVault is not activated on HyperCore, and a HyperEVM -> HyperCore HYPE transfer is made,
         // the transferred HYPE will be lost.
@@ -65,8 +53,6 @@ contract StakingVault is IStakingVault, Base {
             revert NotActivatedOnHyperCore();
         }
         _transfer(payable(HYPE_SYSTEM_ADDRESS), amount);
-
-        lastEvmToCoreTransferBlockNumber = block.number;
     }
 
     /// @inheritdoc IStakingVault
@@ -80,13 +66,8 @@ contract StakingVault is IStakingVault, Base {
     }
 
     /// @inheritdoc IStakingVault
-    function isSpotBalanceSafe() public view returns (bool) {
-        return block.number > lastEvmToCoreTransferBlockNumber;
-    }
-
-    /// @inheritdoc IStakingVault
-    function spotBalance() public view returns (L1ReadLibrary.SpotBalance memory) {
-        return L1ReadLibrary.spotBalance(address(this), HYPE_TOKEN_ID);
+    function spotBalance(uint64 tokenId) external view returns (L1ReadLibrary.SpotBalance memory) {
+        return L1ReadLibrary.spotBalance(address(this), tokenId);
     }
 
     /// @notice Internal function to handle HYPE transfers from the vault.
--- src/interfaces/IStakingVault.sol
+++ src/interfaces/IStakingVault.sol
@@ -16,9 +16,6 @@ interface IStakingVault {
     /// @notice Emitted when HYPE is deposited into the vault (HyperEVM -> HyperEVM)
     event Deposit(address indexed sender, uint256 amount);
 
-    /// @notice Thrown when a transfer to HyperCore cannot be made until the next block
-    error CannotTransferToCoreUntilNextBlock();
-
     /// @dev Deposit HYPE into the vault (HyperEVM -> HyperEVM)
     /// @dev msg.value is the amount of HYPE to deposit (18 decimals)
     function deposit() external payable;
@@ -55,12 +52,7 @@ interface IStakingVault {
     /// @dev Get the delegator summary for the staking vault using HyperCore precompiles
     function delegatorSummary() external view returns (L1ReadLibrary.DelegatorSummary memory);
 
-    /// @dev Check if the HYPE spot balance is safe to use
-    /// @return isSafe The spot balance is unsafe if there was a HyperEVM -> HyperCore transfer earlier in the block.
-    function isSpotBalanceSafe() external view returns (bool);
-
-    /// @dev Get the HYPE spot balance using HyperCore precompiles
-    /// @dev Check isSpotBalanceSafe() to ensure the spot balance is safe to use
-    /// @return spotBalance The spot balance for the given token
-    function spotBalance() external view returns (L1ReadLibrary.SpotBalance memory spotBalance);
+    /// @dev Get the spot balance for the given token for the staking vault using HyperCore precompiles
+    /// @param tokenId The token ID to get the spot balance for (see https://hyperliquid.gitbook.io/hyperliquid-docs/for-developers/api/asset-ids)
+    function spotBalance(uint64 tokenId) external view returns (L1ReadLibrary.SpotBalance memory);
 }
