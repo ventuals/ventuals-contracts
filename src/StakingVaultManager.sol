6bd5a4c28e2b7d089d5627f35427e6f60a657734
Emily Hsia <emily.j.hsia@gmail.com>
2025-10-13 15:42:41 -0400

    feat: Add IStakingVaultManager interface for integrators (#121)

--- src/StakingVaultManager.sol
+++ src/StakingVaultManager.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.27;
 
 import {IStakingVault} from "./interfaces/IStakingVault.sol";
+import {IStakingVaultManager} from "./interfaces/IStakingVaultManager.sol";
 import {L1ReadLibrary} from "./libraries/L1ReadLibrary.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {Base} from "./Base.sol";
@@ -9,169 +10,9 @@ import {VHYPE} from "./VHYPE.sol";
 import {Converters} from "./libraries/Converters.sol";
 import {StructuredLinkedList} from "./libraries/StructuredLinkedList.sol";
 
-contract StakingVaultManager is Base {
+contract StakingVaultManager is Base, IStakingVaultManager {
     using Converters for *;
     using StructuredLinkedList for StructuredLinkedList.List;
-
-    /// @notice Thrown if HYPE transfer fails to given recipient for specified amount.
-    error TransferFailed(address recipient, uint256 amount);
-
-    /// @notice Thrown if an amount of 0 is provided.
-    error ZeroAmount();
-
-    /// @notice Thrown if the deposit amount is below the minimum deposit amount.
-    error BelowMinimumDepositAmount();
-
-    /// @notice Thrown if the withdraw amount is below the minimum withdraw amount.
-    error BelowMinimumWithdrawAmount();
-
-    /// @notice Thrown if the caller is not authorized to perform the action.
-    error NotAuthorized();
-
-    /// @notice Thrown if the withdraw was cancelled.
-    error WithdrawCancelled();
-
-    /// @notice Thrown if the withdraw was not processed.
-    error WithdrawNotProcessed();
-
-    /// @notice Thrown if the withdraw was already processed.
-    error WithdrawProcessed();
-
-    /// @notice Thrown if the withdraw was already claimed.
-    error WithdrawClaimed();
-
-    /// @notice Thrown if the withdraw is not claimable yet.
-    error WithdrawUnclaimable();
-
-    /// @notice Thrown if the batch is not ready to be processed.
-    error BatchNotReady(uint256 readyAt);
-
-    /// @notice Thrown if the batch is invalid.
-    error InvalidBatch(uint256 batch);
-
-    /// @notice Thrown if the batch cannot be slashed.
-    error CannotSlashBatchOutsideSlashWindow(uint256 batch);
-
-    /// @notice Thrown if batch processing is paused.
-    error BatchProcessingPaused();
-
-    /// @notice Thrown if the account balance is less than the reserved HYPE for withdraws.
-    error AccountBalanceLessThanReservedHypeForWithdraws();
-
-    /// @notice Thrown if we don't have enough balance to finalize the batch.
-    error NotEnoughBalance();
-
-    /// @notice Thrown if the updated minimum stake balance is too large.
-    error MinimumStakeBalanceTooLarge();
-
-    /// @notice Thrown if we have more withdraw capacity when a batch is finalized.
-    error HasMoreWithdrawCapacity();
-
-    /// @notice Thrown if trying to finalize before a batch has been processed.
-    error NothingToFinalize();
-
-    /// @notice Thrown if trying to reset a batch that has not been processed.
-    error NothingToReset();
-
-    /// @notice Emitted when HYPE is deposited into the vault
-    /// @param depositor The address that deposited the HYPE
-    /// @param minted The amount of vHYPE minted (in 18 decimals)
-    /// @param deposited The amount of HYPE deposited (in 18 decimals)
-    event Deposit(address indexed depositor, uint256 minted, uint256 deposited);
-
-    /// @notice Emitted when a withdraw is queued
-    /// @param account The address that queued the withdraw
-    /// @param withdrawId The ID of the withdraw
-    /// @param withdraw The withdraw data
-    event QueueWithdraw(address indexed account, uint256 withdrawId, Withdraw withdraw);
-
-    /// @notice Emitted when a withdraw is cancelled
-    /// @param account The address that cancelled the withdraw
-    /// @param withdrawId The ID of the withdraw
-    /// @param withdraw The withdraw data
-    event CancelWithdraw(address indexed account, uint256 withdrawId, Withdraw withdraw);
-
-    /// @notice Emitted when a withdraw is processed
-    /// @param account The address that processed the withdraw
-    /// @param withdrawId The ID of the withdraw
-    /// @param withdraw The withdraw data
-    event ProcessWithdraw(address indexed account, uint256 withdrawId, Withdraw withdraw);
-
-    /// @notice Emitted when a withdraw is claimed
-    /// @param account The address that claimed the withdraw
-    /// @param withdrawId The ID of the withdraw
-    /// @param withdraw The withdraw data
-    event ClaimWithdraw(address indexed account, uint256 withdrawId, Withdraw withdraw);
-
-    /// @notice Emitted when a batch is processed
-    /// @param batchId The ID of the batch
-    /// @param batch The batch data
-    event ProcessBatch(uint256 batchId, Batch batch);
-
-    /// @notice Emitted when a batch is finalized
-    /// @param batchId The ID of the batch
-    /// @param batch The batch data
-    event FinalizeBatch(uint256 batchId, Batch batch);
-
-    /// @notice Emitted when a batch is slashed
-    /// @param batchIndex The index of the batch
-    /// @param slashedExchangeRate The exchange rate after the slash
-    event ApplySlash(uint256 batchIndex, uint256 slashedExchangeRate);
-
-    /// @notice Emitted when a batch is reset
-    /// @param batchIndex The index of the batch
-    /// @param batch The batch data
-    event ResetBatch(uint256 batchIndex, Batch batch);
-
-    /// @notice Emitted when a batch is finalized after being reset
-    /// @param batchIndex The index of the batch
-    /// @param batch The batch data
-    event FinalizeResetBatch(uint256 batchIndex, Batch batch);
-
-    /// @notice Emitted when an emergency staking withdraw is executed
-    /// @param sender The address that executed the emergency withdraw
-    /// @param amount The amount of HYPE withdrawn
-    /// @param purpose The purpose of the withdrawal
-    event EmergencyStakingWithdraw(address indexed sender, uint256 amount, string purpose);
-
-    /// @notice Emitted when an emergency staking deposit is executed
-    /// @param sender The address that executed the emergency deposit
-    /// @param amount The amount of HYPE deposited
-    /// @param purpose The purpose of the deposit
-    event EmergencyStakingDeposit(address indexed sender, uint256 amount, string purpose);
-
-    /// @dev A batch of withdraws that are processed together
-    struct Batch {
-        /// @dev The total amount of withdraws processed in this batch (vHYPE; in 18 decimals)
-        uint256 vhypeProcessed;
-        /// @dev The exchange rate at the time the batch was processed (in 18 decimals)
-        uint256 snapshotExchangeRate;
-        /// @dev The exchange rate if a slash was applied to the batch (in 18 decimals)
-        uint256 slashedExchangeRate;
-        /// @dev Whether the batch was slashed
-        bool slashed;
-        /// @dev The timestamp at which the batch was finalized.
-        uint256 finalizedAt;
-    }
-
-    /// @dev A withdraw from the vault
-    struct Withdraw {
-        /// @dev The ID of the withdraw
-        uint256 id;
-        /// @dev The account that requested the withdraw
-        address account;
-        /// @dev The amount of vHYPE to redeem (in 18 decimals)
-        uint256 vhypeAmount;
-        /// @dev The timestamp at which the withdraw was queued
-        uint256 queuedAt;
-        /// @dev The index of the batch this withdraw was assigned to
-        /// @dev If the withdraw has not been assigned to a batch, this is set to type(uint256).max
-        uint256 batchIndex;
-        /// @dev The timestamp at which the withdraw was cancelled. 0 if not cancelled
-        uint256 cancelledAt;
-        /// @dev The timestamp at which the withdraw was claimed. 0 if not claimed
-        uint256 claimedAt;
-    }
 
     /// forge-lint: disable-next-line(mixed-case-variable)
     VHYPE public vHYPE;
@@ -268,7 +109,7 @@ contract StakingVaultManager is Base {
         claimWindowBuffer = 12 hours;
     }
 
-    /// @notice Deposits HYPE into the vault, and mints the equivalent amount of vHYPE. Refunds any excess HYPE if only a partial deposit is made. Reverts if the vault is full.
+    /// @inheritdoc IStakingVaultManager
     function deposit() external payable canDeposit whenNotPaused {
         uint256 amountToDeposit = msg.value.stripUnsafePrecision();
 
@@ -287,9 +128,7 @@ contract StakingVaultManager is Base {
         emit Deposit(msg.sender, amountToMint, amountToDeposit);
     }
 
-    /// @notice Queues a withdraw from the vault
-    /// @param vhypeAmount The amount of vHYPE to redeem (in 18 decimals)
-    /// @return The IDs of the withdraws
+    /// @inheritdoc IStakingVaultManager
     function queueWithdraw(uint256 vhypeAmount) external whenNotPaused returns (uint256[] memory) {
         require(vhypeAmount > 0, ZeroAmount());
         require(vHYPEtoHYPE(vhypeAmount) >= minimumWithdrawAmount, BelowMinimumWithdrawAmount());
@@ -359,9 +198,7 @@ contract StakingVaultManager is Base {
         return withdrawAmounts;
     }
 
-    /// @notice Claims a withdraw
-    /// @param withdrawId The ID of the withdraw to claim
-    /// @param destination The address to send the HYPE to
+    /// @inheritdoc IStakingVaultManager
     function claimWithdraw(uint256 withdrawId, address destination) public whenNotPaused {
         Withdraw storage withdraw = withdraws[withdrawId];
         require(msg.sender == withdraw.account, NotAuthorized());
@@ -388,17 +225,14 @@ contract StakingVaultManager is Base {
         emit ClaimWithdraw(msg.sender, withdrawId, withdraw);
     }
 
-    /// @notice Claims multiple withdraws
-    /// @param withdrawIds The IDs of the withdraws to claim
-    /// @param destination The address to send the HYPE to
+    /// @inheritdoc IStakingVaultManager
     function batchClaimWithdraws(uint256[] calldata withdrawIds, address destination) public whenNotPaused {
         for (uint256 i = 0; i < withdrawIds.length; i++) {
             claimWithdraw(withdrawIds[i], destination);
         }
     }
 
-    /// @notice Cancels a withdraw. A withdraw can only be cancelled if it has not been processed yet.
-    /// @param withdrawId The ID of the withdraw to cancel
+    /// @inheritdoc IStakingVaultManager
     function cancelWithdraw(uint256 withdrawId) external whenNotPaused {
         Withdraw storage withdraw = withdraws[withdrawId];
         require(msg.sender == withdraw.account, NotAuthorized());
@@ -419,8 +253,7 @@ contract StakingVaultManager is Base {
         emit CancelWithdraw(msg.sender, withdrawId, withdraw);
     }
 
-    /// @notice Processes a batch of withdraws
-    /// @param numWithdrawals The number of withdraws to process
+    /// @inheritdoc IStakingVaultManager
     function processBatch(uint256 numWithdrawals) external whenNotPaused whenBatchProcessingNotPaused {
         Batch memory batch = _fetchBatch();
 
@@ -510,7 +343,7 @@ contract StakingVaultManager is Base {
         return batch;
     }
 
-    /// @dev stores the batch, either by appending a new one or by overwriting the current batch.
+    /// @dev Stores the batch, either by appending a new one or by overwriting the current batch.
     function _checkpointBatch(Batch memory batch) internal {
         if (currentBatchIndex == batches.length) {
             batches.push(batch);
@@ -519,7 +352,7 @@ contract StakingVaultManager is Base {
         }
     }
 
-    /// @notice Finalizes the current batch.
+    /// @inheritdoc IStakingVaultManager
     function finalizeBatch() external whenNotPaused whenBatchProcessingNotPaused {
         // Check if we have a batch to finalize
         require(currentBatchIndex < batches.length, NothingToFinalize());
@@ -606,30 +439,27 @@ contract StakingVaultManager is Base {
         }
     }
 
-    /// @notice Returns the batch at the given index
-    /// @param index The index of the batch to return
+    /// @inheritdoc IStakingVaultManager
     function getBatch(uint256 index) external view returns (Batch memory) {
         return batches[index];
     }
 
-    /// @notice Returns the length of the batches array
+    /// @inheritdoc IStakingVaultManager
     function getBatchesLength() external view returns (uint256) {
         return batches.length;
     }
 
-    /// @notice Returns the withdraw at the given ID
-    /// @param withdrawId The ID of the withdraw to return
+    /// @inheritdoc IStakingVaultManager
     function getWithdraw(uint256 withdrawId) external view returns (Withdraw memory) {
         return withdraws[withdrawId];
     }
 
-    /// @notice Returns the size of the withdraw queue (number of withdraws in the linked list)
+    /// @inheritdoc IStakingVaultManager
     function getWithdrawQueueLength() external view returns (uint256) {
         return withdrawQueue.sizeOf();
     }
 
-    /// @notice Returns the amount of HYPE for the given withdraw ID
-    /// @param withdrawId The ID of the withdraw to return
+    /// @inheritdoc IStakingVaultManager
     function getWithdrawAmount(uint256 withdrawId) external view returns (uint256) {
         Withdraw memory withdraw = withdraws[withdrawId];
         uint256 vhypeAmount = withdraw.vhypeAmount;
@@ -645,9 +475,7 @@ contract StakingVaultManager is Base {
         return _vHYPEtoHYPE(vhypeAmount, _exchangeRate);
     }
 
-    /// @notice Returns the time at which the withdraw will be claimable
-    /// @dev Returns `type(uint256).max` if the withdraw has not been processed yet
-    /// @param withdrawId The ID of the withdraw to return
+    /// @inheritdoc IStakingVaultManager
     function getWithdrawClaimableAt(uint256 withdrawId) external view returns (uint256) {
         Withdraw memory withdraw = withdraws[withdrawId];
         uint256 batchIndex = withdraw.batchIndex;
@@ -663,18 +491,11 @@ contract StakingVaultManager is Base {
         return batch.finalizedAt + 7 days + claimWindowBuffer;
     }
 
-    /// @notice Calculates the vHYPE amount for a given HYPE amount, based on the exchange rate
-    /// @param hypeAmount The HYPE amount to convert (in 18 decimals)
-    /// @return The vHYPE amount (in 18 decimals)
-    /// forge-lint: disable-next-line(mixed-case-function)
+    /// @inheritdoc IStakingVaultManager
     function HYPETovHYPE(uint256 hypeAmount) public view returns (uint256) {
         return _HYPETovHYPE(hypeAmount, exchangeRate());
     }
 
-    /// @notice Calculates the vHYPE amount for a given HYPE amount, based on the provided exchange rate
-    /// @param hypeAmount The HYPE amount to convert (in 18 decimals)
-    /// @param _exchangeRate The exchange rate to use (in 18 decimals)
-    /// @return The vHYPE amount (in 18 decimals)
     /// forge-lint: disable-next-line(mixed-case-function)
     function _HYPETovHYPE(uint256 hypeAmount, uint256 _exchangeRate) internal pure returns (uint256) {
         if (_exchangeRate == 0) {
@@ -683,18 +504,11 @@ contract StakingVaultManager is Base {
         return Math.mulDiv(hypeAmount, 1e18, _exchangeRate);
     }
 
-    /// @notice Calculates the HYPE amount for a given vHYPE amount, based on the exchange rate
-    /// @param vHYPEAmount The vHYPE amount to convert (in 18 decimals)
-    /// @return The HYPE amount (in 18 decimals)
-    /// forge-lint: disable-next-line(mixed-case-function, mixed-case-variable)
+    /// @inheritdoc IStakingVaultManager
     function vHYPEtoHYPE(uint256 vHYPEAmount) public view returns (uint256) {
         return _vHYPEtoHYPE(vHYPEAmount, exchangeRate());
     }
 
-    /// @notice Calculates the HYPE amount for a given vHYPE amount, based on the provided exchange rate
-    /// @param vHYPEAmount The vHYPE amount to convert (in 18 decimals)
-    /// @param _exchangeRate The exchange rate to use (in 18 decimals)
-    /// @return The HYPE amount (in 18 decimals)
     /// forge-lint: disable-next-line(mixed-case-function, mixed-case-variable)
     function _vHYPEtoHYPE(uint256 vHYPEAmount, uint256 _exchangeRate) internal pure returns (uint256) {
         if (_exchangeRate == 0) {
@@ -703,8 +517,7 @@ contract StakingVaultManager is Base {
         return Math.mulDiv(vHYPEAmount, _exchangeRate, 1e18);
     }
 
-    /// @notice Returns the exchange rate of HYPE to vHYPE (in 18 decimals)
-    /// @dev Ratio of total HYPE in the staking vault to vHYPE
+    /// @inheritdoc IStakingVaultManager
     function exchangeRate() public view returns (uint256) {
         uint256 balance = totalBalance();
         uint256 totalSupply = vHYPE.totalSupply();
@@ -722,7 +535,7 @@ contract StakingVaultManager is Base {
         return Math.mulDiv(balance, 1e18, totalSupply);
     }
 
-    /// @notice Returns the total HYPE balance that belongs to the vault (in 18 decimals)
+    /// @inheritdoc IStakingVaultManager
     function totalBalance() public view returns (uint256) {
         // EVM + Spot + Staking account balances
         uint256 accountBalances = stakingAccountBalance() + spotAccountBalance() + stakingVault.evmBalance();
@@ -755,8 +568,7 @@ contract StakingVaultManager is Base {
         return spotBalance.total.to18Decimals();
     }
 
-    /// @notice Returns the withdraws for a given account
-    /// @param account The account to get withdraws for
+    /// @inheritdoc IStakingVaultManager
     function getAccountWithdraws(address account) public view returns (Withdraw[] memory) {
         uint256[] memory withdrawIds = accountWithdrawIds[account];
         Withdraw[] memory _withdraws = new Withdraw[](withdrawIds.length);
@@ -770,8 +582,7 @@ contract StakingVaultManager is Base {
     /*                       Owner Actions                        */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    /// @notice Sets the minimum stake balance (in 18 decimals)
-    /// @dev Minimum stake balance is the total amount of HYPE that must remain staked in the vault
+    /// @inheritdoc IStakingVaultManager
     function setMinimumStakeBalance(uint256 _minimumStakeBalance) public onlyOwner {
         // If we're in the middle of processing a batch, check that we haven't processed more HYPE
         // than what we'd have left after setting the minimum stake balance
@@ -944,8 +755,7 @@ contract StakingVaultManager is Base {
         emit EmergencyStakingDeposit(msg.sender, amount, purpose);
     }
 
-    /// @notice Enables full withdrawal by setting the minimum stake balance to 0
-    /// @dev This is used in the event the LST is wind down
+    /// @inheritdoc IStakingVaultManager
     function windDown() external onlyOwner {
         setMinimumStakeBalance(0);
     }
